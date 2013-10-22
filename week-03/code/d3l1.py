d33a4e5fcffa4c1044059cedd0ea3a3bdd96b75e
Nykhedimus <nykhedimus@gmail.com>
2013-10-22 00:04:18 -0700

    Week 3 Labs - WIP

---
+++ week-03/code/d3l1.py
@@ -0,0 +1,11 @@
+#d3l1.py
+
+arList = (1, 2, 3, 4)
+arList2 = (2, 123.4567, 10000)
+
+def stringFun():
+	print 'the first 3 numbers are: %i, %i, %i' % arList
+
+
+def stringLess():
+	print 'file_00%s : %s, %s' %arList2
