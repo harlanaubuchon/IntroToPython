0c80fb8a9f263fea8ce005fa498cb52434655215
nykhedimus <nyk@x61t>
2013-11-12 18:27:49 -0800

    Updated step 1 week 06 code

--- week-06/code/html_render/htmlRend.py
+++ week-06/code/html_render/htmlRend.py
@@ -1,17 +1,36 @@
 #htmlRend.py
+import sys
 
-class Element(something):
-	fO = '<'
-	fC = '>'
-	tagName = ''
-	indentation = ''
+class Element(object):
 
-    def __init__(self, c):
-    	self.content = c
+	tagName = 'html'
+	indentation = '    '
 
-	def append(self, s):
-		self.content += s
+	def __init__(self, c=None):
+		self.clist = c
+
+	def append(self, e):
+		self.clist = e
 
 	def render(self, file_out, ind=''):
-		self.
+		fo = '<'
+		fc = '>'		
+		file_out.write(ind)
+		file_out.write(fo + self.tagName + fc)
+		file_out.write(self.clist)
+		file_out.write(fo + '/' + self.tagName + fc)
+
+
+if __name__ == "__main__":
+
+	f = open('htmlTest.html', 'w')
+
+	page = Element()
+
+	page.append("Here is a paragraph of text -- there could be more of them, but this is enough  to show that we can do some text")
+
+	page.render(f)
+	
+	print f.read()
+	f.reset()
 
